cc4db96ba90ab6bb4ef005c54685cf8aeed4cf3c
ikori <ikorifey@mail.ru>
2026-03-15 16:26:52 +0300

    code sample

--- text.cpp
+++ text.cpp
@@ -1,31 +1,21 @@
 int main()
 {
-    int i;
-    int j;
-    int k;
-    int n;
-    int a[4]; /* 2x2 = 4 */
-    int b[4];
-    int c[4];
-    
-    n = 2;
-
-    
-    /* Инициализация массивов */
-    for (i = 0; i < 4; i = i + 1) {
+    int i, j, k, n, l, m;
+    int a[10000];
+    int b[10000];
+    int c[10000];
+    n = 100;
+    for(i = 0; i < 10000; i=i+1) {
         a[i] = i % 6;
         b[i] = 1 % 11;
     }
-
-    /* Перемножение матриц */
-    for (i = 0; i < n; i = i + 1) {
-        for (j = 0; j < n; j = j + 1) {
-            c[i * n + j] = 0;
-            for (k = 0; k < n; k = k + 1) {
-                c[i * n + j] = c[i * n + j] + a[i * n + k] * b[k * n + j];
+    for(i = 0; i < n; i=i+1) {
+        for(j = 0; j < n; j=j+1) {
+            c[i*n+j] = 0;
+            for(k = 0; k < n; k=k+1) {
+                c[i*n+j] = c[i*n+j] + a[i*n+k] * b[k*n+j];
             }
         }
     }
-
     return 0;
 }
